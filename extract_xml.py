d7eba219feb2115da75af81b649e85a1fa173726
EnekoDev <enekolabdev@gmail.com>
2026-02-16 10:58:40 +0100

    First commit

--- extract_xml.py
+++ extract_xml.py
@@ -1,36 +1,28 @@
+import os
 import xml.etree.ElementTree as ET
 import csv
 
-XML_URL = '/home/eneko/Documentos/workspaces/arratek-workspace/aplicaciones/sme-app/wetransfer_archivos-pendientes-del-sindicato-medico-de-gipuzkoa_2025-01-29_0948/Afiliados SMG a 28-01-2025 - B.Sabadell.XML'
-CSV_URL = '/home/eneko/Documentos/workspaces/arratek-workspace/aplicaciones/extract_xml/iban.csv'
+XML_URL = os.environ['XML_URL', '']
+CSV_URL = os.environ['CSV_URL', '']
 
-# Define the namespace
-ns = {'ns': 'urn:iso:std:iso:20022:tech:xsd:pain.008.001.02'}
 
-# Load XML file
-tree = ET.parse(XML_URL)  # Update with your actual file path
+namespace = {'ns': 'urn:iso:std:iso:20022:tech:xsd:pain.008.001.02'}
+
+tree = ET.parse(XML_URL)
 root = tree.getroot()
 
-# Open CSV file for writing
 with open(CSV_URL, 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     
-    # Write header row
     writer.writerow(["Debtor Name", "IBAN"])
     
-    # Find all direct debit transactions
-    for drct_dbt_tx in root.findall(".//ns:DrctDbtTxInf", ns):
-        # Extract Debtor Name
-        nm_element = drct_dbt_tx.find(".//ns:Dbtr/ns:Nm", ns)
+    for drct_dbt_tx in root.findall(".//ns:DrctDbtTxInf", namespace):
+        nm_element = drct_dbt_tx.find(".//ns:Dbtr/ns:Nm", namespace)
         debtor_name = nm_element.text.strip() if nm_element is not None and nm_element.text else "Not Provided"
 
-        # Extract IBAN
-        iban_element = drct_dbt_tx.find(".//ns:DbtrAcct/ns:Id/ns:IBAN", ns)
+        iban_element = drct_dbt_tx.find(".//ns:DbtrAcct/ns:Id/ns:IBAN", namespace)
         iban = iban_element.text.strip() if iban_element is not None and iban_element.text else "Not Provided"
 
-        # Debugging: Print extracted values to check
         print(f"Extracted: Name = '{debtor_name}', IBAN = '{iban}'")
 
-        # Write to CSV
         writer.writerow([debtor_name, iban])
-
